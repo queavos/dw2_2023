b4638f3062847069cbfe63b66ec802773bb74140
queavos <queavos.dev@gmail.com>
2023-03-29 18:43:53 -0400

    tarea demo

--- 0322/js/app.js
+++ 0322/js/app.js
@@ -1,3 +1,4 @@
+/*
 console.log("Hola Mundo!!!");
 var variable=1;
 console.log(variable);
@@ -39,13 +40,39 @@ function cliqueo ()
     for (var i=0; i<ele.length; i++ )
     {
         console.log(ele[i]);
+        clg
         ele[i].classList.add("text-danger");
     }
 
     // ele.forEach(e => {
     //     e;
     //  });
+    clg
     
     alert("CORRE!");
-}
+} 
+*/
+var  frutas=["manzana","pera"];
+console.log(frutas[0]);
+frutas[2]="naranja";// inserto elemento por el indice
+console.log(frutas);
+frutas.push("frutilla");// inserta elemento al final del array
+console.log(frutas);
+console.log(frutas.indexOf("pera")); //devuelve el indice de un valor
+frutas.unshift("mandarina"); // agrega elemento al inicio del array
+console.log(frutas);
+frutas.shift(); // elimina un elemento al final del array
+console.log(frutas.length); // muesta la cantidad de elementos del array
+frutas.pop(); // elimina elemento al final del array 
+console.log(frutas);
 
+function suma(a,b) { return a+b; } // define funcion
+console.log(suma(10,5));
+const resta = (a,b) => { return a-b;} // define arrow function
+//resta=[];
+console.log(resta(10,5));
+frutas.forEach((item,i)=>{  //recorre los elementos de un array y ejecuta una funcion
+console.log("index es "+i+" y el valor es "+ item );
+ });
+ frutas[1]="pomelo";
+ console.log(frutas);
